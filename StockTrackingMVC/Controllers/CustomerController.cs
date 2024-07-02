220e79fdba4ce77ea59ae8534a8501f1fac079dc
f4t1hsalih <f4t1hsalih@gmail.com>
2024-07-02 19:01:02 +0300

    Lecture 75 - Müşteri Controller ve Index Tanımlama

---
+++ StockTrackingMVC/Controllers/CustomerController.cs
@@ -0,0 +1,19 @@
+﻿using StockTrackingMVC.Models.Entity;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace StockTrackingMVC.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            using (DB_StockTrackingMVCEntities db = new DB_StockTrackingMVCEntities())
+            {
+                var customers = db.tbl_customers.ToList();
+                return View(customers);
+            }
+        }
+    }
+}
