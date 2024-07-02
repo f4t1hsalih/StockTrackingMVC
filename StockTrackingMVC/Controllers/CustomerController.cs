26ea212bb1cdb108a4ff007d2d1c706f74005b11
f4t1hsalih <f4t1hsalih@gmail.com>
2024-07-02 19:45:40 +0300

    Lecture 78 - Yeni Müşteri Ekleme

--- StockTrackingMVC/Controllers/CustomerController.cs
+++ StockTrackingMVC/Controllers/CustomerController.cs
@@ -9,13 +9,30 @@ namespace StockTrackingMVC.Controllers
     public class CustomerController : Controller
     {
         // GET: Customer
-        public ActionResult Index(int sayfa = 1)
+        public ActionResult Index(int page = 1)
         {
             using (DB_StockTrackingMVCEntities db = new DB_StockTrackingMVCEntities())
             {
                 //var customers = db.tbl_customers.ToList();
-                var customerList = db.tbl_customers.ToList().ToPagedList(sayfa, 10);
+                var customerList = db.tbl_customers.ToList().ToPagedList(page, 10);
                 return View(customerList);
+            }
+        }
+
+        // AddCustomer
+        [HttpGet]
+        public ActionResult AddCustomer()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddCustomer(tbl_customers customers)
+        {
+            using (DB_StockTrackingMVCEntities db = new DB_StockTrackingMVCEntities())
+            {
+                db.tbl_customers.Add(customers);
+                db.SaveChanges();
+                return RedirectToAction("Index");
             }
         }
     }
