238f0a44e4b578da986475612c57ddcebd818100
f4t1hsalih <f4t1hsalih@gmail.com>
2024-07-02 19:14:58 +0300

    Lecture 76 - Müşterilerde Sayfalama İşlemi

--- StockTrackingMVC/Controllers/CustomerController.cs
+++ StockTrackingMVC/Controllers/CustomerController.cs
@@ -1,18 +1,21 @@
 ﻿using StockTrackingMVC.Models.Entity;
 using System.Linq;
 using System.Web.Mvc;
+using PagedList;
+using PagedList.Mvc;
 
 namespace StockTrackingMVC.Controllers
 {
     public class CustomerController : Controller
     {
         // GET: Customer
-        public ActionResult Index()
+        public ActionResult Index(int sayfa = 1)
         {
             using (DB_StockTrackingMVCEntities db = new DB_StockTrackingMVCEntities())
             {
-                var customers = db.tbl_customers.ToList();
-                return View(customers);
+                //var customers = db.tbl_customers.ToList();
+                var customerList = db.tbl_customers.ToList().ToPagedList(sayfa, 3);
+                return View(customerList);
             }
         }
     }
