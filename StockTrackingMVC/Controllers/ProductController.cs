cad16b9e27fa30c3097c9a5bb192394c5c82eae1
f4t1hsalih <f4t1hsalih@gmail.com>
2024-07-02 16:56:27 +0300

    Lecture 65 - Ürün Listeleme

---
+++ StockTrackingMVC/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using StockTrackingMVC.Models.Entity;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace StockTrackingMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            using (DB_StockTrackingMVCEntities db = new DB_StockTrackingMVCEntities())
+            {
+                var products = db.tbl_products.Include("tbl_categories").ToList();
+                return View(products);
+            }
+        }
+    }
+}
