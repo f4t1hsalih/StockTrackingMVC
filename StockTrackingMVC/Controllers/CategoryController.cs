7a32eb78779ef0a6f5d8afd8db9cca4e523cb527
f4t1hsalih <f4t1hsalih@gmail.com>
2024-07-02 15:35:49 +0300

    Lecture 63-64 - Kategori Güncelleme İşlemi

--- StockTrackingMVC/Controllers/CategoryController.cs
+++ StockTrackingMVC/Controllers/CategoryController.cs
@@ -45,5 +45,27 @@ namespace StockTrackingMVC.Controllers
                 return RedirectToAction("Index");
             }
         }
+
+        //GET: Category/UpdateCategory
+        [HttpGet]
+        public ActionResult UpdateCategory(int id)
+        {
+            using (DB_StockTrackingMVCEntities db = new DB_StockTrackingMVCEntities())
+            {
+                var category = db.tbl_categories.Find(id);
+                return View(category);
+            }
+        }
+        [HttpPost]
+        public ActionResult UpdateCategory(tbl_categories category)
+        {
+            using (DB_StockTrackingMVCEntities db = new DB_StockTrackingMVCEntities())
+            {
+                var updatedCategory = db.tbl_categories.Find(category.ctg_id);
+                updatedCategory.ctg_name = category.ctg_name;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+        }
     }
 }
