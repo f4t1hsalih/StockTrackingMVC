cf47d386b675cd70a01527efe61cdadef5f2477b
f4t1hsalih <f4t1hsalih@gmail.com>
2024-07-03 13:19:11 +0300

    Kategori Kısmına Validation Eklendi

--- StockTrackingMVC/Controllers/CategoryController.cs
+++ StockTrackingMVC/Controllers/CategoryController.cs
@@ -16,57 +16,80 @@ namespace StockTrackingMVC.Controllers
             }
         }
 
-
         // GET: Category/AddCategory
         [HttpGet]
         public ActionResult AddCategory()
         {
             return View();
         }
+
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult AddCategory(tbl_categories category)
         {
-            using (DB_StockTrackingMVCEntities db = new DB_StockTrackingMVCEntities())
+            if (ModelState.IsValid)
             {
-                category.ctg_status = true;
-                db.tbl_categories.Add(category);
-                db.SaveChanges();
+                using (DB_StockTrackingMVCEntities db = new DB_StockTrackingMVCEntities())
+                {
+                    category.ctg_status = true;
+                    db.tbl_categories.Add(category);
+                    db.SaveChanges();
+                }
                 return RedirectToAction("Index");
             }
+            return View(category);
         }
 
-        //GET: Category/DeleteCategory
+        // GET: Category/DeleteCategory
         public ActionResult DeleteCategory(int id)
         {
             using (DB_StockTrackingMVCEntities db = new DB_StockTrackingMVCEntities())
             {
                 var category = db.tbl_categories.Find(id);
+                if (category == null)
+                {
+                    return HttpNotFound();
+                }
                 category.ctg_status = false;
                 db.SaveChanges();
-                return RedirectToAction("Index");
             }
+            return RedirectToAction("Index");
         }
 
-        //GET: Category/UpdateCategory
+        // GET: Category/UpdateCategory
         [HttpGet]
         public ActionResult UpdateCategory(int id)
         {
             using (DB_StockTrackingMVCEntities db = new DB_StockTrackingMVCEntities())
             {
                 var category = db.tbl_categories.Find(id);
+                if (category == null)
+                {
+                    return HttpNotFound();
+                }
                 return View(category);
             }
         }
+
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult UpdateCategory(tbl_categories category)
         {
-            using (DB_StockTrackingMVCEntities db = new DB_StockTrackingMVCEntities())
+            if (ModelState.IsValid)
             {
-                var updatedCategory = db.tbl_categories.Find(category.ctg_id);
-                updatedCategory.ctg_name = category.ctg_name;
-                db.SaveChanges();
+                using (DB_StockTrackingMVCEntities db = new DB_StockTrackingMVCEntities())
+                {
+                    var updatedCategory = db.tbl_categories.Find(category.ctg_id);
+                    if (updatedCategory == null)
+                    {
+                        return HttpNotFound();
+                    }
+                    updatedCategory.ctg_name = category.ctg_name;
+                    db.SaveChanges();
+                }
                 return RedirectToAction("Index");
             }
+            return View(category);
         }
     }
 }
