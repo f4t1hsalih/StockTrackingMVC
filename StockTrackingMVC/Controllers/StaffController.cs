241a8a3f12de50f14054dce3c3dd0f7d6ab59cf9
f4t1hsalih <f4t1hsalih@gmail.com>
2024-07-04 23:01:42 +0300

    Personel Ekleme Controller ve View Kodlandı

--- StockTrackingMVC/Controllers/StaffController.cs
+++ StockTrackingMVC/Controllers/StaffController.cs
@@ -11,10 +11,66 @@ namespace StockTrackingMVC.Controllers
         {
             using (DB_StockTrackingMVCEntities db = new DB_StockTrackingMVCEntities())
             {
-                var staff = db.tbl_staff.ToList();
+                var staff = db.tbl_staff.Where(x => x.stf_status != false).ToList();
                 return View(staff);
             }
-
         }
+
+        // AddStaff
+        [HttpGet]
+        public ActionResult AddStaff()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddStaff(tbl_staff staff)
+        {
+            if (ModelState.IsValid)
+            {
+                using (DB_StockTrackingMVCEntities db = new DB_StockTrackingMVCEntities())
+                {
+                    staff.stf_status = true;
+                    db.tbl_staff.Add(staff);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            else
+            {
+                return View(staff);
+            }
+        }
+
+        // UpdateStaff
+        [HttpGet]
+        public ActionResult UpdateStaff(int id)
+        {
+            using (DB_StockTrackingMVCEntities db = new DB_StockTrackingMVCEntities())
+            {
+                var staff = db.tbl_staff.Find(id);
+                return View(staff);
+            }
+        }
+        [HttpPost]
+        public ActionResult UpdateStaff(tbl_staff staff)
+        {
+            if (ModelState.IsValid)
+            {
+                using (DB_StockTrackingMVCEntities db = new DB_StockTrackingMVCEntities())
+                {
+                    var updatedStaff = db.tbl_staff.Find(staff.stf_id);
+                    updatedStaff.stf_name = staff.stf_name;
+                    updatedStaff.stf_surname = staff.stf_surname;
+                    updatedStaff.stf_department = staff.stf_department;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            else
+            {
+                return View(staff);
+            }
+        }
+
     }
 }
