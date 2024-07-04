86b20be0bf412d0c7bb92c34088093e6c77a4ab8
f4t1hsalih <f4t1hsalih@gmail.com>
2024-07-04 22:51:49 +0300

    Personel Listeleme Eklendi

---
+++ StockTrackingMVC/Controllers/StaffController.cs
@@ -0,0 +1,20 @@
+﻿using StockTrackingMVC.Models.Entity;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace StockTrackingMVC.Controllers
+{
+    public class StaffController : Controller
+    {
+        // GET: Staff
+        public ActionResult Index()
+        {
+            using (DB_StockTrackingMVCEntities db = new DB_StockTrackingMVCEntities())
+            {
+                var staff = db.tbl_staff.ToList();
+                return View(staff);
+            }
+
+        }
+    }
+}
