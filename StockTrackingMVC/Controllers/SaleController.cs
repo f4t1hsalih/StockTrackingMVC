62abb7544dc8894f7c890b5c47bc7eb5d0a8a6fe
f4t1hsalih <f4t1hsalih@gmail.com>
2024-07-04 14:05:24 +0300

    Lecture 84 - Satış Controller

---
+++ StockTrackingMVC/Controllers/SaleController.cs
@@ -0,0 +1,24 @@
+﻿using StockTrackingMVC.Models.Entity;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace StockTrackingMVC.Controllers
+{
+    public class SaleController : Controller
+    {
+        // GET: Sale
+        public ActionResult Index()
+        {
+            using (DB_StockTrackingMVCEntities db = new DB_StockTrackingMVCEntities())
+            {
+                var sales = db.tbl_sales
+              .Include(s => s.tbl_products)
+              .Include(s => s.tbl_staff)
+              .Include(s => s.tbl_customers)
+              .ToList();
+                return View(sales);
+            }
+        }
+    }
+}
--- StockTrackingMVC/Models/Entity/tbl_sales.cs
+++ StockTrackingMVC/Models/Entity/tbl_sales.cs
@@ -15,11 +15,11 @@ namespace StockTrackingMVC.Models.Entity
     public partial class tbl_sales
     {
         public int sal_id { get; set; }
-        public Nullable<int> sal_prd_id { get; set; }
-        public Nullable<int> sal_stf_id { get; set; }
-        public Nullable<int> sal_ctm_id { get; set; }
-        public Nullable<decimal> sal_salePrice { get; set; }
-        public Nullable<System.DateTime> sal_date { get; set; }
+        public int sal_prd_id { get; set; }
+        public int sal_stf_id { get; set; }
+        public int sal_ctm_id { get; set; }
+        public decimal sal_salePrice { get; set; }
+        public System.DateTime sal_date { get; set; }
     
         public virtual tbl_customers tbl_customers { get; set; }
         public virtual tbl_products tbl_products { get; set; }
